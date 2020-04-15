33056f386c814dc61bae448cf8aec1468bbf3d35
Freezblade <62288786+Freezblade@users.noreply.github.com>
2020-04-15 13:04:23 +0200

    Update R_code_Lezione2_Spatial.r

--- R_code_Lezione2_Spatial.r
+++ R_code_Lezione2_Spatial.r
@@ -1,4 +1,6 @@
 # R SPATIAL, LEZIONE 2-ECOLOGIA DEL PAESAGGIO
+#English version below
+
 
 #chiamiamo il pacchetto sp
 library(sp)
@@ -42,5 +44,46 @@ spplot(meuse,"zinc")
 # il plot che è uscito è di tipo spaziale i punti gialli ci vanno ad indicare le zone più inquinate, stiamo analizzando le zone vicine ad un fiume.
 
 
+#English
 
+# let's call the package sp
+library (sp)
+
+#use the data command to open our dataframe
+date (meuse)
+
+#with head we see the first 6
+head (meuse)
+
+#use attach to fix the dataframe
+attach (meuse)
+
+# let's make a plot to correlate cadmium and lead
+plot (cadmium, lead, main = "Cadmium and lead relationship", col = "red", pch = 18, xlab = "Cadmium", ylab = "Lead")
+
+#ecercise: create a plot with copper and zinc
+plot (copper, zinc, main = "Relationship between Copper and Zinc", col = "green", pch = 17, xlab = "Copper", ylab = "Zinc")
+
+#to show the graphs together we use: multiframe or multipanel
+par (mfrow = c (1,2))
+plot (cadmium, lead, main = "Cadmium and lead relationship", col = "red", pch = 18, xlab = "Cadmium", ylab = "Lead")
+plot (copper, zinc, main = "Relationship between Copper and Zinc", col = "green", pch = 17, xlab = "Copper", ylab = "Zinc")
+# if we change the mfrow values ​​to 2.1 we will have a variation in the graph display
+
+#install through packages, install, GGally or externally with install.packages ("GGally"), once installed we call the library
+library (GGally)
+
+#ggpairs (meuse) plots all variables, we use ggpairs (meuse [, 3: 6]) to take only the variables of interest
+ggpairs (meuse [3: 6])
+
+#SPATIAL
+#explain to R that there are coordinates in the data
+coordinates (meuse) = ~ x + y
+
+#we see the graph with:
+plot (meuse)
+
+#to create the spatial chart use this function:
+spplot (meuse, "zinc")
+# the plot that came out is spatial type the yellow points go to indicate the most polluted areas, we are analyzing the areas close to a river.
 
